994a504a9febd332a72d92f19d3411bc54062914
Chas Lemley <chas.lemley@gmail.com>
2014-04-23 13:55:22 -0400

    clean up Configuration implementation

--- lib/fastly-rails/configuration.rb
+++ lib/fastly-rails/configuration.rb
@@ -1,47 +1,21 @@
 module FastlyRails
-
   class Configuration
-
-    attr_reader :api_key, :user, :password, :max_age
-
     MAX_AGE_DEFAULT = '2592000'
 
-    class << self
+    attr_accessor :api_key, :user, :password, :max_age
 
-      def max_age_default
-        MAX_AGE_DEFAULT
-      end
-
+    def self.max_age_default
+      MAX_AGE_DEFAULT
     end
 
     def initialize
-      @api_key   = nil
-      @user      = nil
-      @password  = nil
-      @max_age   = self.class.max_age_default
-    end
-
-    def api_key=(val)
-      @api_key = val
-    end
-
-    def user=(val)
-      @user = val
-    end
-
-    def password=(val)
-      @password = val
-    end
-
-    def max_age=(val)
-      @max_age = val
+      @max_age = MAX_AGE_DEFAULT
     end
 
     def authenticatable?
-      !api_key.nil? || (!user.nil? && !password.nil?)
+      return true if api_key
+
+      !!(user && password)
     end
-
   end
-
 end
-
